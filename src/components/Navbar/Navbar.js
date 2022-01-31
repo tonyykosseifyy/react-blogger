42abc3994e62ecad7ddaa47938b84da0a97b4715
tonyykosseifyy <tonykosseify123@gmail.com>
2022-01-31 21:06:41 +0200

    3 files modified, 1 added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-
+      <img src={require("../../assets/logo.svg")} alt="Blogr Logo" />
     </nav>
   );
 };
